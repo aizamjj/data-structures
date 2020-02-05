a821c8fbdc937c76611b1761bc179436f68d61d2
Aizam Jigjidsuren <aizamjig@gmail.com>
2020-02-05 15:02:52 -0800

    Has working solution but not all tests pass.

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,27 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var count = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    // add the value to the storage after the last one
+    count++;
+    storage[count] = value;
   };
 
-  someInstance.dequeue = function() {
+  someInstance.dequeue = function(value) {
+    
+    // removes the first one and returns it
+    count--;
+    var result = storage[count];
+    delete storage[count];
+    return result;
   };
 
-  someInstance.size = function() {
+  someInstance.size = function(value) {
+    return count;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,17 +1,26 @@
 var Stack = function() {
   var someInstance = {};
-
   // Use an object with numeric keys to store values
   var storage = {};
-
+  // Create a count variable to keep track of the 
+  var count = 0;
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[count] = value;
+    count++;
   };
 
-  someInstance.pop = function() {
+  someInstance.pop = function(value) {
+    count--;
+    var result = storage[count];
+    // delete that element
+    delete storage[count];
+    // return that element
+    return result;
   };
-
-  someInstance.size = function() {
+  
+  someInstance.size = function(value) {
+    return count;
   };
 
   return someInstance;
