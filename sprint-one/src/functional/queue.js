f9812b020a2cadf6f9fb1781fe35733cc5730834
Aizam Jigjidsuren <aizamjig@gmail.com>
2020-02-06 07:41:57 -0800

    Implemented the methods for stack and queue data structures. Passed all the tests.

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -2,29 +2,48 @@ var Queue = function() {
   var someInstance = {};
 
   // Use an object with numeric keys to store values
-  var storage = {};
-  var count = 0;
-
-  // Implement the methods below
+  var storage = {}; // property within Queue
+  var head = 0;
+  var tail = 0;
 
   someInstance.enqueue = function(value) {
-    // add the value to the storage after the last one
-    count++;
-    storage[count] = value;
+    // store the given value on the tail end of the queue
+    storage[tail] = value;
+    // increment tail
+    tail++;
   };
 
-  someInstance.dequeue = function(value) {
-    
-    // removes the first one and returns it
-    count--;
-    var result = storage[count];
-    delete storage[count];
+  someInstance.dequeue = function() {
+    // grab the first item in the queue (head = 0 and tail = 1)
+    var result = storage[head];
+    // delete the first item (head = no longer exists, tail is still 1)
+    delete storage[head];
+    // what happens when we dequeue right off the bat when head and tail are at 0
+
+    head < tail && head++;
     return result;
   };
 
-  someInstance.size = function(value) {
-    return count;
+  someInstance.size = function() {
+
+    return tail - head;
   };
 
   return someInstance;
 };
+
+//Process:
+
+//Specification: I - value (data type: string) O - someInstance (object);
+  // Constraints:
+  // Edge Cases:
+//Justification: The purpose of the function is to store the given
+  // value at the end of the storage object
+  // when Queue is invoked, it creates an instance of the class
+  // class
+//Explanation
+  // the relationship between the input and output: there is n
+//Visualization
+//Approximation
+//Verification
+//Implementation
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -2,24 +2,25 @@ var Stack = function() {
   var someInstance = {};
   // Use an object with numeric keys to store values
   var storage = {};
-  // Create a count variable to keep track of the 
+  // Create a count variable to keep track of how big the stack is
   var count = 0;
-  // Implement the methods below
+
   someInstance.push = function(value) {
-    storage[count] = value;
-    count++;
+    storage[count] = value; // value = 4; storage: {0: 4} count = 0; 
+    count++; // count = 1;
   };
 
-  someInstance.pop = function(value) {
-    count--;
-    var result = storage[count];
-    // delete that element
-    delete storage[count];
-    // return that element
+  someInstance.pop = function() {
+    // count is truthy, then decrement count
+    count && count--;
+    var result = storage[count]; // result = 4
+    // delete the element
+    delete storage[count]; //result = undefined, count is undefined
+    // return the element
     return result;
   };
   
-  someInstance.size = function(value) {
+  someInstance.size = function() {
     return count;
   };
 
