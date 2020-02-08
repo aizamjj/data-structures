03e408f3991c90debe37803a2c2f58b48099ba20
aizamjj <aizamjig@gmail.com>
2020-02-07 22:13:45 -0800

    Accomplish Tree Problem

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,19 +3,38 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];
+  // newTree.addChild = treeMethods.addChild;
+  // newTree.contains = treeMethods.contains;
+  _.extend(newTree, treeMethods);
 
   return newTree;
 };
 
+
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  this.children.push(Tree(value));
 };
+
 
 treeMethods.contains = function(target) {
+  if (this.value === target) {
+    return true;
+  }
+
+  for (var i = 0; i < this.children.length; i++) {
+    if (this.children[i].contains(target) === true) {
+      return true;
+    }
+  }
+
+
+  return false;
 };
 
+// var tree = Tree();
 
 
 /*
